4832c35da21ff37dd3e4dbe0138e9cd6bcfff995
Matteo Savarese <matteo.savarese@ulb.be>
2024-03-01 09:25:17 +0100

    Modified example

--- Example/run.m
+++ Example/run.m
@@ -1,7 +1,14 @@
 %% Make sure all the functions are in the path
 addpath(genpath('/Users/matteosavarese/Desktop/Dottorato/Github/NetGEN'));
 
+set(0, 'defaultaxesfontsize', 20);
+set(0, 'defaulttextfontsize', 20);
+set(0, 'defaulttextinterpreter', 'latex');
 %% Define options
+clear all;
+
+% Path to data
+opt_global.DataPath = '/Users/matteosavarese/Desktop/Dottorato/Github/NetGEN/Example/DataCFD/';
 
 % Data import options
 opt_global.VolumeFile = 'data_volumes';
@@ -13,16 +20,12 @@ opt_global.TVarianceFile = 'data_Trms';
 opt_global.VelocityFile = 'data_velocity';
 opt_global.BoundaryCellsFile = 'Boundary_cells';
 opt_global.SolutionFile = 'data_solution';
-% opt_global.ViscosityFile = 'data_viscosity';
 opt_global.Basis = 'mol';
 
 % Simulation options
-opt_global.KineticMech = 'gri2.11'; % gri3.0, gri2.11, SanDiego, Polimi, Stagni, Otomo
-opt_global.PostProcessing = false;
-opt_global.SolveEnergy = false;
+opt_global.KineticMech = 'SanDiego'; % gri3.0, gri2.11, SanDiego, Polimi, Stagni, Otomo
+opt_global.SolveEnergy = true;
 opt_global.RunSimulation = true;
-opt_global.PlotContour = false;
-opt_global.PlotProfile = false;
 opt_global.KineticCorrections = true;
 opt_global.DataVariance = true;
 opt_global.InitComp = true;
@@ -34,29 +37,49 @@ opt_global.Scale = 'auto';
 
 % K-Means options
 opt_global.Alg = 'k-means';
-opt_global.Start = 'uniform';
+opt_global.Start = 'plus';
 opt_global.MaxIter = 1000;
 
 % Write output file 
 opt_global.WriteFile = true;
 
 % Options for reassigning nodes
-opt_global.VolumeThreshold = 0.01;
+opt_global.VolumeThreshold = 0.001;
 opt_global.ReassignCriterion = 'volume';
-% Re-assign options
 opt_global.Criterion = 'Euclidean';
-opt_global.ReassignCriterion = 'volume';
-opt_global.VolumeThreshold = 0.01;
 
-% % Diffusion between reactors
-% opt_global.Diffusion = true;
+% Options for reassigning clusters
+opt_global.ClusterReassignment = true;
+opt_global.ClusterReassignmentCriterion = 'volumes';
 
-% Path to data
-opt_global.DataPath = '/Users/matteosavarese/Desktop/Dottorato/Github/NetGEN/Example/';
+% Reordering reactors
+opt_global.ReorderReactors = true;
 
 %% Run the simulation
 load case_info.mat
-k = 10;
-ndim = 2;
+
+% Select number of clusters
+k = 8;     % Number of clusters
+ndim = 2;   % If case is 2D or 3D
+
+fname1 = append('k-means_', num2str(k), 'clust_', opt_global.Scale, '_', date);
+fname2 = append('k-means_', num2str(k), 'clust_', opt_global.Scale, '_', date, '_', opt_global.KineticMech);
+fold2 = append(fname1, '/', fname2, '/Output/');
+
 [mass_corrected, bc_mass_flowrates, R_list, idx, X, infos] = ...
-    network_eval(k, ndim, inlet_streams, case_info, opt_global);
+     network_eval(k, ndim, inlet_streams, case_info, opt_global);
+
+%% Plot the clustering results
+load(append(fname1, '/', 'clustering_results.mat'))
+data_all = import_data_all(opt_global.DataPath, ndim, opt_global);
+
+figure;
+scatter(data_all.Coordinates(:,1), data_all.Coordinates(:,2), 10, idx, 'filled');
+cb = colorbar;
+ax = gca; ax.TickLabelInterpreter = 'latex';
+xlabel('x [m]'); ylabel('y [m]');
+fig = gcf; fig.Units = 'centimeters';
+fig.Position = [15 15 18 12];
+
+
+
